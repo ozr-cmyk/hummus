03ade8939db5e62a95c16e45305b1aaa07c63b1b
ozr-cmyk <ozrafer@gmail.com>
2025-11-19 14:36:30 -0800

    Add files via upload

---
+++ hummus.js
@@ -0,0 +1,11 @@
+function myFunction() {
+  document.getElementById("garlic").innerHTML="No garlic!";
+}
+function rateMe() {
+    document.getElementById("rate").innerHTML="Thanks!";
+}
+
+function lebnanYou() {
+    let x
+    document.getElementById("lebnan").innerHTML="Lebanon has the best hummus!"
+}
